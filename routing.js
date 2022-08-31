cf5548eaa7c5038444c53ae6d716d3b108a281a8
Steven Gu Shiwen <99197394+gushiwen@users.noreply.github.com>
2022-08-31 11:10:27 +0800

    Add files via upload

---
+++ routing.js
@@ -0,0 +1,23 @@
+// This is a function that takes a page path as an argument
+// You need to make the function load the content of that page into the "content" div on the index file
+async function loadPage(page) {
+  // fetch the page, use await
+  const res = await fetch(page);
+  // get text from res, use await
+  const content = await res.text();
+  // get the element with id 'content'
+  const element = document.getElementById('content');
+  // set innerHTML of the element
+  // your code goes here
+  element.innerHTML = content;
+}
+
+loadPage('signin.html');
+
+function route() {
+  link = location.hash.replace('#','');
+  console.log(link);
+  loadPage(link);
+}
+
+window.addEventListener('hashchange', route);
